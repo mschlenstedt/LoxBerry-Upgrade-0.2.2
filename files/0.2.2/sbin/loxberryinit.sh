9f758a2e88260312cee5a048d79b4246b4bc0565
Michael Schlenstedt <michael@loxberry.de>
2016-09-17 14:46:27 +0200

    First commit

---
+++ files/0.2.2/sbin/loxberryinit.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          loxberry
+# Required-Start:    $remote_fs $syslog $network 
+# Required-Stop:     $remote_fs $syslog $network
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: This file starts special things needed by Loxberry.
+# Description:       This file starts special things needed by Loxberry.
+### END INIT INFO
+
+# Copyright 2016 Michael Schlenstedt, michael@loxberry.de
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Version 1.5
+# 16.09.2016 14:26:00
+
+PATH="/sbin:/bin:/usr/sbin:/usr/bin:/opt/loxberry/bin:/opt/loxberry/sbin"
+
+. /lib/lsb/init-functions
+
+case "$1" in
+  start|"")
+        # Resize rootfs to maximum if not yet done
+        if [ ! -f /boot/rootfsresized ]
+        then
+          log_action_begin_msg "Resizing Rootfs to maximum on next reboot"
+          /opt/loxberry/sbin/resize_rootfs > /dev/null 2>&1
+          touch /boot/rootfsresized
+        fi
+
+        # Copy manual network configuration if any exists
+        if [ -f /boot/network.txt ]
+        then
+          log_action_begin_msg "Found manual network configuration in /boot. Activating..."
+          mv /opt/loxberry/system/network/interfaces  /opt/loxberry/system/network/interfaces.bkp > /dev/null 2>&1
+          cp /boot/network.txt  /opt/loxberry/system/network/interfaces > /dev/null 2>&1
+          dos2unix /etc/network/interfaces > /dev/null 2>&1
+          mv /boot/network.txt /boot/network.bkp > /dev/null 2>&1
+          /etc/init.d/networking restart > /dev/null 2>&1
+        fi
+
+        # Copy new HTACCESS User/Password Database
+        if [ -f /opt/loxberry/config/system/htusers.dat.new ]
+        then
+          log_action_begin_msg "Found new htaccess password database. Activating..."
+          mv /opt/loxberry/config/system/htusers.dat.new /opt/loxberry/config/system/htusers.dat > /dev/null 2>&1
+        fi
+
+        # Do a system upgrade
+        if [ -f /opt/loxberry/data/system/upgrade/upgrade.sh ]
+        then
+          log_action_begin_msg "Found system upgrade. Installing..."
+          /opt/loxberry/data/system/upgrade/upgrade.sh > /opt/loxberry/log/system/upgrade.log 2>&1
+        fi
+
+        # Cleaning Temporary folders
+        log_action_begin_msg "Cleaning temporary files and folders..."
+        rm -rf /opt/loxberry/webfrontend/html/tmp/* > /dev/null 2>&1
+
+        # Set Date and Time
+        if [ -f /opt/loxberry/sbin/setdatetime.pl ]
+        then
+          log_action_begin_msg "Syncing Date/Time with Miniserver or NTP-Server"
+          su loxberry -c "/opt/loxberry/sbin/setdatetime.pl > /dev/null 2>&1"
+        fi
+
+        # Run Daemons from Plugins and from System
+        log_action_begin_msg "Running System Daemons"
+        run-parts -v /opt/loxberry/system/daemons/system > /dev/null 
+
+        log_action_begin_msg "Running Plugin Daemons"
+        run-parts -v /opt/loxberry/system/daemons/plugins > /dev/null 
+
+        # Run Uninstall scripts
+        log_action_begin_msg "Running Uninstall Scripts"
+        run-parts -v /opt/loxberry/system/daemons/uninstall > /dev/null 
+        rm -rf /opt/loxberry/system/daemons/uninstall/* > /dev/null 2>&1
+
+	;;
+	restart|reload|force-reload|status)
+        echo "Error: argument '$1' not supported" >&2
+        exit 3
+  ;;
+  stop)
+        # No-op
+        ;;
+  *)
+        echo "Usage: $0 [start|stop]" >&2
+        exit 3
+  ;;
+esac
---
+++ upgrade.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+#
+# LoxBerry Upgrade Script
+#
+
+# Script basedir
+DIR=$(dirname $(readlink -f $0))
+echo "<INFO> My basedir: $DIR"
+cd $DIR
+
+# Current Version
+if [ -f "../../../config/system/general.cfg" ]
+	then
+
+	VERSION=`cat ../../../config/system/general.cfg | grep VERSION= | xargs -d '=' | awk '{print $2}' | head -1`
+	NVERSION=`echo $VERSION | sed 's/\.//g'` # string with two dots cannot be compared in bash
+	echo "<INFO> Current LoxBerry Version: $VERSION"
+
+else
+
+  echo "<FAIL>Cannot find your system configuration. Aborting."
+  exit 1;
+
+fi
+
+##################################################
+# Upgrade 0.2.1 to 0.2.2 / Upgrade 0.2.2 to 0.2.2
+##################################################
+if [ "$NVERSION" -eq "021" ] || [ "$NVERSION" -eq "022" ]
+  then
+
+  echo "<INFO>Upgrade will be done from $VERSION to 0.2.2"
+
+  cp -v -r ./files/0.2.2/data/system/uninstall ../../../data/system/
+  chown -v -R loxberry.loxberry ../../../data/system/uninstall
+
+  cp -v ./files/0.2.2/sbin/* ../../../sbin/
+  chown -v -R loxberry.loxberry ../../../sbin  
+  chmod -v 755 ../../../sbin/*
+
+  cp -v -r ./files/0.2.2/system/daemons/uninstall ../../../system/daemons/
+  chown -v -R loxberry.loxberry ../../../system/daemons/uninstall
+
+  cp -v ./files/0.2.2/templates/system/timezones.dat ../../../templates/system/
+  cp -v ./files/0.2.2/templates/system/de/help/* ../../../templates/system/de/help/
+  cp -v ./files/0.2.2/templates/system/de/setup/* ../../../templates/system/de/setup/
+  cp -v ./files/0.2.2/templates/system/de/* ../../../templates/system/de/
+  cp -v ./files/0.2.2/templates/system/en/help/* ../../../templates/system/en/help/
+  cp -v ./files/0.2.2/templates/system/en/setup/* ../../../templates/system/en/setup/
+  cp -v ./files/0.2.2/templates/system/en/* ../../../templates/system/en/
+  chown -v -R loxberry.loxberry ../../../templates/system
+  
+  cp -v -r ./files/0.2.2/webfrontend/cgi/system/setup ../../../webfrontend/cgi/system/
+  cp -v ./files/0.2.2/webfrontend/cgi/system/* ../../../webfrontend/cgi/system/
+  chown -v -R loxberry.loxberry ../../../webfrontend/cgi/system
+  chmod -v 755 ../../../webfrontend/cgi/system/*
+  chmod -v 755 ../../../webfrontend/cgi/system/setup/*
+
+  echo "<OK>Upgrade successfully done to Version 0.2.2"
+
+  exit 0
+
+else
+
+  echo "<FAIL>Cannot find any upgrade for your version. Your version is $VERSION. Aborting."
+  exit 1
+
+fi
+
+exit 0
